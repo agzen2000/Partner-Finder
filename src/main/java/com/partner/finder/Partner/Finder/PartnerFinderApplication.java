c1c25311033c68dff25c60ce17dc89c531817350
agzen2000 <akashsen.as00@gmail.com>
2020-10-17 18:27:42 -0700

    Initial commit

---
+++ src/main/java/com/partner/finder/Partner/Finder/PartnerFinderApplication.java
@@ -0,0 +1,13 @@
+package com.partner.finder.Partner.Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partner/finder/Partner/Finder/PartnerFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partner.finder.Partner.Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
